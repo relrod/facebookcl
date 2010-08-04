a854e767721ac9791339c81165fe2383befdb3a9
Rick Elrod <ricky@elrod.me>
2010-08-04 13:19:54 -0400

    Friends...kind of ish work? Not complete yet, but committing what I have so far.

---
+++ commands/friends.rb
@@ -0,0 +1,28 @@
+require 'pp'
+module FacebookCommands
+  class << self
+    def friends(*args)
+      if args.first == 'help'
+        puts 'list [returns list of your friends]'
+        puts 'list <uid> [returns list of <uid>\'s friends]'
+        return
+      end
+      
+      if args.first == 'list'
+        args.shift
+        person = !args.empty? ? args.first : 'me'
+        friends = FacebookCL.get("#{person}/friends")
+        friends['data'].each do |friend|
+          puts "[*] #{friend['name']}"
+        end
+      end
+      
+      if args.first == 'count'
+        args.shift
+        person = !args.empty? ? args.first : 'me'
+        friends = FacebookCL.get("#{person}/friends")
+        puts "Count: #{friends['data'].size}"
+      end
+    end
+  end
+end
