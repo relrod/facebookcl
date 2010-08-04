42dddd792c072503fc0e4b787758e48bbb107910
Rick Elrod <ricky@elrod.me>
2010-08-04 10:45:15 -0400

    Error handling that actually works -- also provides 'like list post_id'

--- commands/like.rb
+++ commands/like.rb
@@ -5,11 +5,18 @@ module FacebookCommands
       if args.first == 'help'
         puts 'like <post id>'
         return
+      elsif args.first == 'list'
+        args.shift
+        likes = FacebookCL.get("#{args.first}/likes")
+        likes['data'].each do |user|
+          puts "[*] #{user['name']} (#{user['id']})"
+        end
+        return
       end
       
       like = FacebookCL.post("#{args.first}/likes")
 
-      if not defined? like['error']
+      if like.class.to_s == 'Net::HTTPOK'
         puts "You like post id #{args.first}."
       else
         puts "You were unable to successfully like post id #{args.first}."
